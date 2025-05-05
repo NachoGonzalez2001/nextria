15f5ac8a02d6e96be944142cabaabf80aa05e3f9
Mathias Pessaj <mathipess@gmail.com>
2025-05-05 13:20:00 -0300

    Cambio de dependencias y un estilo

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -21,7 +21,7 @@ export default function Navbar() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 items-center">
-        <Link href="/" className="mr-6 flex items-center space-x-2">
+        <Link href="/" className="mr-6 ml-5 flex items-center space-x-2">
           <span className="hidden font-bold sm:inline-block">React con Next.js</span>
         </Link>
         <div className="hidden md:flex md:flex-1 md:items-center md:justify-between">
